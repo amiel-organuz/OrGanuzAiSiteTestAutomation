3a6ecd1ff63d46e3b921e67a393c302d7e052f1a
amielpeled <amielnoy@gmail.com>
2026-06-12 11:34:49 +0300

    swagger & prometeus invoked from docker-compose

--- server/app/main.py
+++ server/app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(
     title="OrGanuz Test Automation API",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080", "http://127.0.0.1:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 Instrumentator().instrument(app).expose(app, endpoint="/metrics")
